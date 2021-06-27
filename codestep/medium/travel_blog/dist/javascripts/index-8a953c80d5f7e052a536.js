c9e19eb393ffc8c29b8e2b40c30d8572a042f04b
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-27 21:47:59 +0900

    add styles #1

---
+++ codestep/medium/travel_blog/dist/javascripts/index-8a953c80d5f7e052a536.js
@@ -0,0 +1,67 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/stylesheets/index.scss":
+/*!************************************!*\
+  !*** ./src/stylesheets/index.scss ***!
+  \************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n  ╷\n6 │     a }\n  │       ^\n  ╵\n  src/stylesheets/object/component/_nav.scss 6:7  @import\n  src/stylesheets/index.scss 35:9                 root stylesheet\n    at processResult (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/webpack/lib/NormalModule.js:701:19)\n    at /Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/webpack/lib/NormalModule.js:807:5\n    at /Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/loader-runner/lib/LoaderRunner.js:399:11\n    at /Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/loader-runner/lib/LoaderRunner.js:251:18\n    at context.callback (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at /Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/sass-loader/dist/index.js:54:7\n    at Function.call$2 (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/sass/sass.dart.js:93417:16)\n    at _render_closure.call$0 (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/sass/sass.dart.js:81756:23)\n    at Object.Primitives_applyFunction (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/sass/sass.dart.js:1124:30)\n    at Object.Function_apply (/Users/raido/Desktop/develop/code_practice/practice_coding/codestep/medium/travel_blog/node_modules/sass/sass.dart.js:5894:16)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************************!*\
+  !*** ./src/javascripts/index.js ***!
+  \**********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _stylesheets_index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../stylesheets/index.scss */ "./src/stylesheets/index.scss");
+ // css-loaderがないと読み込めない
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=index-8a953c80d5f7e052a536.js.map
--- codestep/medium/travel_blog/webpack.config.js
+++ codestep/medium/travel_blog/webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
       },
       {
         // image section
-        test: /\.(png|jpe?g)/,
+        test: /\.(png|jpe?g|svg)/,
         type: "asset/resource",
         generator: {
           filename: "images/[name]-[contenthash][ext]",
