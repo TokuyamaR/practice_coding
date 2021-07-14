a5acdbf10961955658129d89a3ceb82985509e7a
TokuyamaR <qpflapper.1007@gmail.com>
2021-07-15 08:30:13 +0900

    add initial files for fa_exihibition

---
+++ codestep/high/fa_exhibition/src/javascripts/carousel/index.js
@@ -0,0 +1,19 @@
+import "slick-carousel";
+import "slick-carousel/slick/slick.scss";
+import "slick-carousel/slick/slick-theme.scss";
+
+$(".pickup-items").slick({
+  arrows: false,
+  centerMode: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
---
+++ codestep/high/fa_exhibition/webpack.config.js
@@ -0,0 +1,95 @@
+// pathモジュールの読み込み
+const path = require("path");
+// mini-css-extract-pluginモジュールの読み込み
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// htmlファイルを生成。読み込んだjs, cssファイルも読み込まれる
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// clean-webpack-pluginモジュールの読み込み
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Webpack = require("webpack");
+
+module.exports = {
+  devtool: "source-map",
+  //  エントリポイント
+  entry: {
+    index: "./src/javascripts/index.js",
+  },
+  // アウトプット先
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "javascripts/[name]-[contenthash].js",
+    publicPath: "./",
+  },
+  // モジュール設定
+  module: {
+    rules: [
+      {
+        // js section
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              // pluginが束になったもの
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "> 0.25%, not dead", // 対象ブラウザを指定
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        // css section
+        test: /\.(css|scss|sass)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader, // jsファイルに読み込まれたcssを別ファイルに切り離す
+          },
+          {
+            loader: "css-loader", // cssファイルを読み込む
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: "sass-loader", // sassファイルを読み込み、cssへコンパイルする
+          },
+        ],
+      },
+      {
+        // image section
+        test: /\.(png|jpe?g|svg|ico|mp4|gif|eot|woff|ttf)/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[contenthash][ext]",
+          publicPath: "/dist/",
+        },
+        use: [
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/[name]-[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html",
+      filename: "index.html",
+    }),
+    // 自動生成されるファイル以外の不要ファイルを削除(outputで指定したディレクトリ配下を対象とする)
+    new CleanWebpackPlugin(),
+    new Webpack.ProvidePlugin({
+      $: "jquery",
+    }),
+  ],
+};
